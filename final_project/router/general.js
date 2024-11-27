554a44834f02c1aa61c8e672f4d68bb88f48cfc6
ueberhagender <34742643+ueberhagender@users.noreply.github.com>
2024-11-27 14:59:56 +0100

    Asynchronous functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,51 +24,81 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  res.send(books);
+  let myPromise = new Promise((resolve) => {
+    resolve(books);
+  });
+
+  myPromise.then((promisedBooks) => {
+    res.send(promisedBooks);
+  });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  let isbn = req.params.isbn;
-  if (isbn) {
+  let myPromise = new Promise((resolve, reject) => {
+    let isbn = req.params.isbn;
     if (Object.keys(books).includes(isbn)) {
-      res.send(books[isbn]);
+      resolve(books[isbn]);
     } else {
-      res.send("There is no book associated with ISBN " + isbn + ".");
+      reject("There is no book associated with ISBN " + isbn + ".");
     }
-  } else {
-    res.status(406).json({
-      message: "Request malformed.",
+  });
+
+  myPromise
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((e) => {
+      res.status(404).json(e);
     });
-  }
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  let author = req.params.author;
-  let existingBooks = Object.values(books).filter(
-    (book) => book.author === author
-  );
+  let myPromise = new Promise((resolve, reject) => {
+    let author = req.params.author;
+    let existingBooks = Object.values(books).filter(
+      (book) => book.author === author
+    );
 
-  if (existingBooks.length > 0) {
-    res.send({ booksbyauthor: existingBooks });
-  } else {
-    res.send("No books found by author " + author + ".");
-  }
+    if (existingBooks.length > 0) {
+      resolve({ booksbyauthor: existingBooks });
+    } else {
+      reject("No books found by author " + author + ".");
+    }
+  });
+
+  myPromise
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((e) => {
+      res.status(404).json(e);
+    });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  let title = req.params.title;
-  let existingBooks = Object.values(books).filter(
-    (book) => book.title === title
-  );
+  let myPromise = new Promise((resolve, reject) => {
+    let title = req.params.title;
+    let existingBooks = Object.values(books).filter(
+      (book) => book.title === title
+    );
 
-  if (existingBooks.length > 0) {
-    res.send({ booksbytitle: existingBooks });
-  } else {
-    res.send("No books with title " + title + " exist.");
-  }
+    if (existingBooks.length > 0) {
+      resolve({ booksbytitle: existingBooks });
+    } else {
+      reject("No books with title " + title + " exist.");
+    }
+  });
+
+  myPromise
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((e) => {
+      res.status(404).json(e);
+    });
 });
 
 //  Get book review
